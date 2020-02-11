745682cfda151259f5f88aace73a9c861af48f8b
Celine / Celisoft <celine.liberal@mail.com>
2020-02-11 21:50:19 +0100

    Init base flask app

---
+++ croissantine/__init__.py
@@ -0,0 +1,1 @@
+from .croissantine import app
---
+++ croissantine/croissantine.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "<h1>Croissantine !</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
