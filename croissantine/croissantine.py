15b1b1b53936ab2325cb1c1986376c4a76ad16c7
Celine / Celisoft <celine.liberal@mail.com>
2020-02-13 23:27:55 +0100

    Add navbar + integrate bg + demo page started

--- croissantine/croissantine.py
+++ croissantine/croissantine.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template('index.html')
+
+
+@app.route("/demo")
+def demo():
+    return render_template('demo.html')
 
 
 if __name__ == "__main__":
